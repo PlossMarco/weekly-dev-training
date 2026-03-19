1cacd6b03b8e2676e8e105876e05a8a5a22c5f1b
Marco Ploss <marco-ploss@web.de>
2026-03-19 21:33:06 +0100

    neues Modul: Passwort Checker

--- week01/solution/password.py
+++ week01/solution/password.py
@@ -3,15 +3,33 @@ This module checks if a password is correct or not.
 """
 
 
-def password_checker():
-    """This function checks if a password is correct or not."""
-    password = "Hallo123!"
-    if input("Passwort: ") == password:
-        print("Password is correct.")
-        return True
-    else:
-        print("Password is incorrect.")
+def set_password():
+    """This function sets a password."""
+    password = input("Passwort festlegen: ")
+
+    if len(password) < 8:
+        print("Dein Passwort muss mindestens 8 Zeichen lang sein!")
+        return False
+    
+    for zeichen in password:
+        if zeichen.isdigit():
+            print("Passwort erfolgreich festgelegt.")
+            return True
+        print("Dein Passwort muss mindestens eine Zahl enthalten!")
         return False
 
+def check_password(password):
+    """This function checks if a password is correct or not."""
+    if input("Passwort: ") == password:
+        print("Passwort ist richtig.")
+        return True
+    else:
+        print("Passwort ist falsch")
+        return False
+    
+
 if __name__ == "__main__":
-    password_checker()
+    password = set_password()
+    if password:
+        check_password(password)
+
